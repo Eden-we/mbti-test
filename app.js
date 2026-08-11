736c5ecbbe433c421de7fd49f3b5a68529b8b270
Eden-we <lix608482@gmail.com>
2026-08-11 21:42:32 +0800

    更新 app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const $ = (id) => document.getElementById(id);
-const ASSET_VERSION = "20260811-4";
+const ASSET_VERSION = "20260811-5";
 const screens = {
   start: $("screen-start"),
   test: $("screen-test"),
